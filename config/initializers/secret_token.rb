7e4fb19b69f98b9012327bcb47b8b30d06e2dba7
pavel <pavel@ubuntu.(none)>
2014-06-11 13:18:47 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scary::Application.config.secret_key_base = 'cd16869185cd5d392f112171ebf5b7a4e26b6a68a3af8546c6a8fce007666a5c9cde1ef41448f82913e1190b57e20c8f1b317ea2fecdedec912d82b48a707731'
