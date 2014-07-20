de8ea879fce03d9da416e55f571edd6697104d24
pavel <pavel@ubuntu.(none)>
2014-07-20 20:53:53 +0400

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,34 @@
 class User < ActiveRecord::Base
 
+	attr_accessor :password_confirmation
+
 	has_many :comments
 
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 
-	validates :name, presence: true, length: { maximum: 50 }
+	validates :name, presence: { message: "Заполните поле 'Имя'" }
+
+	validates :name, length: { maximum: 50,
+		message: "- Длина имени не должна превышать 50 символов"},
+		allow_blank: true
+	
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true,
-				format: { with: VALID_EMAIL_REGEX },
-				uniqueness: { case_sensitive: false } #uniqueness: true
-	has_secure_password
-	validates :password, length: { minimum: 6 }
+	validates :email, presence: { message: "Заполните поле 'Email'" }
+	validates :email,
+				format: { with: VALID_EMAIL_REGEX, message: "Email введен неверно" },
+				uniqueness: { case_sensitive: false }, #uniqueness: true
+				allow_blank: true
+	
+	has_secure_password validations: false
+	validates :password, 
+		presence: { message: "Заполните поле 'Пароль'" }
+	validates_confirmation_of :password, message: "Заполните поле 'Повтор пароля'"
+	validates :password, length: { minimum: 6,
+		message: "- Длина пароля должна быть более 6 символов" },
+		allow_blank: true
+	
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
