236fd375fab4260f4749365b3b51b00b99925467
pavel <pavel@ubuntu.(none)>
2014-07-24 21:59:29 +0400

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,25 @@
+class LabellingFormBuilder < ActionView::Helpers::FormBuilder
+  	def label(method, text = nil, options = {}, &block)
+  		lv_required = @object.class.validators_on(method).map(&:class).include? ActiveRecord::Validations::PresenceValidator		
+  	
+  		lv_label = @template.label(@object_name, method, text, objectify_options(options), &block)
+  		lv_label += "*" if lv_required
+  		return lv_label
+	end
+end
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  include SessionsHelper
+
+	before_action :set_locale
+
+  	protect_from_forgery with: :exception
+
+  	include SessionsHelper
+ 
+	def set_locale
+  		I18n.locale = params[:locale] || "ru"
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,26 @@
 class User < ActiveRecord::Base
 
-	attr_accessor :password_confirmation
-
 	has_many :comments
 
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 
-	validates :name, presence: { message: "Заполните поле 'Имя'" }
+	validates :name, presence: true
 
-	validates :name, length: { maximum: 50,
-		message: "- Длина имени не должна превышать 50 символов"},
+	validates :name, length: { maximum: 50 },
 		allow_blank: true
 	
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: { message: "Заполните поле 'Email'" }
+	validates :email, presence: true
 	validates :email,
-				format: { with: VALID_EMAIL_REGEX, message: "Email введен неверно" },
+				format: { with: VALID_EMAIL_REGEX },
 				uniqueness: { case_sensitive: false }, #uniqueness: true
 				allow_blank: true
 	
-	has_secure_password validations: false
-	validates :password, 
-		presence: { message: "Заполните поле 'Пароль'" }
-	validates_confirmation_of :password, message: "Заполните поле 'Повтор пароля'"
-	validates :password, length: { minimum: 6,
-		message: "- Длина пароля должна быть более 6 символов" },
+	has_secure_password
+	
+	validates :password, length: { minimum: 6 },
 		allow_blank: true
 	
 
